65b5e1b60906e2b4152a30cd67b0a8e700c9e530
Lateef Azeez <lateefazeez@Lateefs-MacBook-Pro.local>
2023-12-27 21:37:42 -0700

    add initial code and configurations

---
+++ deepwave/lib/main_prod.dart
@@ -0,0 +1,8 @@
+import 'package:deepwave/global.dart';
+import 'package:flutter/material.dart';
+import 'package:deepwave/app_entry.dart';
+
+void main() {
+  Global.baseURL = 'https://deepwave.app/prod';
+  runApp(const MyApp());
+}
