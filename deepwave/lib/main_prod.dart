dad319d9ade7fa66b1bfc5a6e33b09b37f38ce90
Lateef Azeez <lateefazeez@Lateefs-MacBook-Pro.local>
2023-12-29 13:27:26 -0700

    replace initial code

--- deepwave/lib/main_prod.dart
+++ deepwave/lib/main_prod.dart
@@ -1,6 +1,6 @@
 import 'package:deepwave/global.dart';
+import 'package:deepwave/my_app.dart';
 import 'package:flutter/material.dart';
-import 'package:deepwave/app_entry.dart';
 
 void main() {
   Global.baseURL = 'https://deepwave.app/prod';
--- deepwave/lib/main_staging.dart
+++
@@ -1,8 +0,0 @@
-import 'package:deepwave/global.dart';
-import 'package:flutter/material.dart';
-import 'package:deepwave/app_entry.dart';
-
-void main() {
-  Global.baseURL = 'https://deepwave.app/staging';
-  runApp(const MyApp());
-}
